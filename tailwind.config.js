7971a110130d958211e3b99d6a8609000c0a4eba
davebroc <davebrockbank02@gmail.com>
2024-07-07 15:39:05 +1200

    tailwind configured

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [react()],
+}
